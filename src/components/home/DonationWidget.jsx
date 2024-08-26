e9b17d5d90fd4645020f0f254db2734e9eeecaf6
fperreault <perreault.francois@gmail.com>
2024-08-25 21:32:24 -0400

    add inview donation to async loading scripts

--- src/components/home/DonationWidget.jsx
+++ src/components/home/DonationWidget.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { spacings } from '@styles/configs/spacings';
+
+import { useInView } from '@hooks/useInView';
+
+import { LoadingCircle } from '@components/generics/LoadingCircle';
 
 /**
  * Donate Widget
  */
 const DonationWidget = () => {
-  return <DonateWidgetWrapper>Donation Script Widget</DonateWidgetWrapper>;
+  const ref = useRef(null);
+  const [ready, setReady] = useState(false);
+  const widgetView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (widgetView) {
+      // TODO: remove this temporary fake loading
+      setTimeout(() => {
+        setReady(true);
+      }, 1000);
+    }
+  }, [widgetView]);
+
+  return (
+    <DonateWidgetWrapper ref={ref}>
+      {!ready ? <LoadingCircle /> : 'Donation bloc here ...'}
+    </DonateWidgetWrapper>
+  );
 };
 
 /**
  * Donate Widget Wrapper
  */
 const DonateWidgetWrapper = styled.div`
+  position: relative;
   width: 312px;
   height: 512px;
   color: ${({ theme }) => theme.colors.black};
