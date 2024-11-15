202a02304e3281540c2d97e5159a50e53f50aa68
Olivier Leger <olivierleger@gmail.com>
2024-11-15 17:04:11 -0500

    Add new Privacy and Terms pages

---
+++ src/pages/privacy/fr.js
@@ -0,0 +1,134 @@
+/*
+ * MEPP - A web application to guide patients and clinicians in the process of
+ * facial palsy rehabilitation, with the help of the mirror effect and principles
+ * of motor learning
+ * Copyright (C) 2021 MEPP <info@mirroreffectplus.org>
+ *
+ * This file is part of MEPP.
+ *
+ * MEPP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MEPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MEPP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import {BR, H3, H5, H6, LI, P, UL} from '@components/generics/basics';
+
+
+const LocalizedPrivacy = () => {
+  return (
+    <>
+      <H3>POLITIQUE DE VIE PRIVÉE</H3>
+
+      <P>
+          Cette politique de vie privée s’applique à la plate-forme MEPP
+          (ci-après «&nbsp;<b>MEPP</b>&nbsp;»), qui comprend un site web
+          (pour les cliniciens et les patients) et une application sur iOS
+          (réservée aux patients).
+      </P>
+
+      <P>MEPP a été développée par la professeure Sarah Martineau et son équipe, avec le soutien de l’Université de Montréal, du CIUSSS de l’Est-de-l’Île-de-Montréal et du CIUSSS du Nord-de-l’Île-de-Montréal («&nbsp;<b>nous</b>&nbsp;»).</P>
+
+      <P>La présente politique décrit lorsqu’applicable la collecte et le traitement qui peuvent être faits des renseignements personnels des utilisateurs de MEPP («&nbsp;<b>vous</b>&nbsp;»).</P>
+
+      <P><b>En utilisant MEPP et son contenu, vous reconnaissez avoir lu et compris cette politique de vie privée.</b></P>
+
+      <H5>1. COLLECTE ET TRAITEMENT NÉCESSAIRES</H5>
+
+      <P><b>La communication de certains renseignements personnels est requise pour utiliser MEPP et son contenu.</b> Par ailleurs, nous ne diffusons pas de publicité sur MEPP. Enfin, nous recueillons des informations dans le but de vous contacter pour vous faire parvenir des messages importants à propos de MEPP, tels que des messages relatifs à la sécurité de MEPP et à la mise à jour de cette politique.</P>
+
+      <H5>2. RENSEIGNEMENTS QUE VOUS DIVULGUEZ</H5>
+
+      <H6>2.1 Lorsque vous utilisez MEPP</H6>
+
+      <P>
+        MEPP demande aux utilisateurs de divulguer pour la création de leur compte :
+        <b>(i)</b> leur nom complet; <b>(ii)</b> leur adresse courriel; <b>(iii)</b> pour les patients, le côté de leur paralysie faciale.
+      </P>
+
+      <P>Pour les patients, lorsque vous accédez à MEPP, nous colligeons des informations sur
+        l’appareil utilisé, telles que les identifiants uniques de l’appareil et les informations issues des capteurs (caméra, microphones, accéléromètres, gyroscopes, boussoles, etc.). De plus, nous enregistrons des fichiers journaux (« log ») qui comprennent certaines informations telles que votre adresse IP, la date et l’heure de votre utilisation de MEPP, des témoins qui identifient votre navigateur et la langue de celui-ci, ainsi que les diverses requêtes aux serveurs de MEPP et les réponses de ceux-ci.</P>
+
+      <P>Si vous consentez à divulguer ces renseignements, alors nous nous engageons :</P>
+      <UL>
+        <LI>à en préserver la confidentialité;</LI>
+        <LI>à les colliger de façon à ce que nous ne puissions pas vous identifier;</LI>
+        <LI>à les utiliser uniquement sous ce format, à des fins de recherche scientifique et d’enseignement (par nos chercheurs responsables et leurs collaborateurs) et à des fins de statistiques et de développement de MEPP, et ce, afin d’améliorer les approches de rééducation de la paralysie faciale et leur efficacité. Aucune de ces utilisations ne sera faite dans le but (ou de manière) à vous identifier.</LI>
+      </UL>
+
+      <P>Tant que vous conserverez votre compte, nous conserverons les renseignements pendant un délai de cinq (5) ans suivant votre dernière connexion à MEPP.</P>
+
+      <H6>2.2 Questions et commentaires</H6>
+
+      <P>Si vous nous contactez, nous aurons alors accès aux informations que vous nous partagerez, incluant vos coordonnées, et toute question posée ou tout commentaire formulé. Nous allons alors recueillir ces informations pour vous répondre et, au besoin, réagir à la situation identifiée.</P>
+
+      <H6>2.3 Droit d’accès et de rectification</H6>
+
+      <P>Vous pouvez recevoir communication de tout renseignement personnel vous concernant. Vous pouvez également en demander la rectification. Certains de vos renseignements personnels peuvent être consultés et corrigés en vous connectant à votre compte et en accédant à votre profil. Autrement, nous vous invitons à nous contacter.</P>
+
+      <H5>3. TECHNOLOGIES</H5>
+
+      <P>Certaines technologies de MEPP sont liées aux renseignements généraux de votre appareil afin d’assurer la cohérence et la sécurité de votre session. Ces technologies sont requises pour que MEPP fonctionne correctement, et seront automatiquement supprimées si vous supprimez MEPP. N’hésitez pas à communiquer avec nous pour toute question.</P>
+
+      <H5>4. MESURES DE SÉCURITÉ</H5>
+
+      <P>Nous mettons en place les mesures raisonnablement requises pour préserver la sécurité et la confidentialité de tout renseignement personnel. À cet effet, nous ne conservons ces renseignements que dans un format qui ne nous permet pas de vous identifier. De plus, nos plateformes et serveurs sont hébergés au Canada. Même si nous utilisons des technologies jugées adéquates, MEPP et son contenu – comme toute technologie – n’est pas infaillible ou à l’abri d’événements imprévisibles. Vous devez être conscient qu’il existe un risque en lien avec l’utilisation de toute technologie. Nous ne pouvons pas garantir que votre accès ou utilisation de MEPP ou de son contenu ne fera pas l’objet d’une interception de données ou autre événement incontrôlable.</P>
+
+      <H5>5. QUESTIONS</H5>
+
+      <P>Vous pouvez communiquer avec nous si vous avez des questions en lien avec nos pratiques de vie privée.</P>
+
+      <H5>6. HYPERLIENS</H5>
+
+      <P>MEPP et son contenu incluent des liens vers d’autres ressources. Ces hyperliens sont fournis à titre informatif. Toutefois, nous vous rappelons :</P>
+      <UL>
+        <LI>que ces ressources ne sont pas contrôlées par nous;</LI>
+        <LI>que nous ne validons pas systématiquement leur contenu;</LI>
+        <LI>que nous ne pouvons pas garantir leur contenu.</LI>
+      </UL>
+
+      <P>Ces ressources sont sujettes à leurs propres conditions d’utilisation et politiques de vie privée. Nous n’assumons aucune responsabilité en lien avec celles-ci. La décision de consulter ces hyperliens vous revient.</P>
+
+      <H5>7. AUTRES TERMES</H5>
+
+      <H6>7.1 Accord complet</H6>
+
+      <P>Cette politique de vie privée constitue, avec les conditions d’utilisation, l’ensemble des règles applicables à votre utilisation de MEPP et du contenu. À noter que les conditions d’utilisation et cette politique sont téléchargeables sur le site Internet de MEPP.</P>
+
+      <H6>7.2 Modifications</H6>
+
+      <P>Nous pouvons modifier cette politique de vie privée. Ces modifications seront portées à votre attention à l’avance. Nous comprendrons que vous acceptez ces changements si vous accédez ou continuez d’utiliser MEPP ou son contenu; si vous n’êtes pas d’accord avec ces modifications, vous devez cesser d’utiliser MEPP ou supprimer l’application de votre appareil avant que ces changements n’entrent en vigueur.</P>
+
+      <H6>7.3 Lois applicables et disputes</H6>
+
+      <P>MEPP, son contenu et cette politique de vie privée sont sujets aux lois en vigueur au Québec à l’exclusion de tout principe pouvant mener à l’application de lois étrangères. Toute dispute sera soumise aux tribunaux de la Province de Québec, qui auront compétence exclusive pour la régler.</P>
+
+      <H6>7.4 Communications</H6>
+
+      <P>Vous pouvez communiquer avec nous via les coordonnées suivantes :</P>
+
+      <P>Adresse :
+        <address>
+          École d’orthophonie et d’audiologie <BR />
+          7077, avenue du Parc, local 3001-1<BR />
+          Montréal (Québec) H3N 1X7
+        </address>
+      </P>
+
+      <P>Courriel&nbsp;:{' '}<a href="sarah.martineau.2@umontreal.ca">sarah.martineau.2@umontreal.ca</a></P>
+      <P>Tél&nbsp;:&nbsp;514 343-7645</P>
+
+      <P>Nous allons donner suite à toute communication aussi rapidement que possible, et au plus tard dans les délais prévus aux lois applicables.</P>
+    </>
+  );
+};
+
+export default LocalizedPrivacy;
